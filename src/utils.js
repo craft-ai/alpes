87e76eb0940bd68fa17ab0530f4401733dfd0f62
Clodéric Mars <cloderic.mars@craft.ai>
2018-01-04 09:41:47 +0100

    Fix the skipped test and simplify 'wrapInPromise'

--- src/utils.js
+++ src/utils.js
@@ -3,13 +3,7 @@
 function wrapInPromise<Output>(f: (...args: Array<any>) => Promise<Output> | Output): (...args: Array<any>) => Promise<Output> {
   return (...args: Array<any>) => {
     try {
-      const result = f(...args);
-      if (result instanceof Promise) {
-        return result;
-      }
-      else {
-        return Promise.resolve(result);
-      }
+      return Promise.resolve(f(...args));
     }
     catch (error) {
       return Promise.reject(error);
