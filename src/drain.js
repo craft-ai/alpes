c6a941b109c1e973fd933ff6279f69cca3025fbd
Alexis Fontaine <alexis.fontaine@isep.fr>
2018-02-27 17:22:23 +0100

    Remove the unnecessary check for `done` in `drain`

--- src/drain.js
+++ src/drain.js
@@ -4,21 +4,16 @@ const { transduce } = require('./basics');
 import type { Stream } from './basics';
 
 function drain<T>(): (Stream<T>) => Promise<void> {
-  const nothing: void = undefined;
   return transduce(
     undefined,
     (accumulation, event) => {
       if (event.error) {
         throw event.error;
       }
-      else if (event.done) {
-        return { accumulation, done: true };
-      }
-      else {
-        return { accumulation, done: false };
-      }
+
+      return { accumulation, done: event.done };
     },
-    () => nothing);
+    () => {});
 }
 
 module.exports = {
