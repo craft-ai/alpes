35d29c405275e3a352fddbee7a15787f4966900d
Clodéric Mars <cloderic.mars@craft.ai>
2018-03-22 09:05:00 +0100

    Using a fixed size array in 'batch'

--- src/batch.js
+++ src/batch.js
@@ -6,15 +6,17 @@ import type { Transformer } from './transduce';
 
 function createBatchTransformer<T, AccumulationT>(count: number): Transformer<T, T[], AccumulationT> {
   return (reducer) => {
-    let currentBatch = undefined;
+    const batch = new Array(count > 0 ? count : 0); // The batch that is being filled up
+    let batchedCount = 0; // The number of already batched events
+
     return (accumulation, event) => {
       if (event.error) {
         return reducer(accumulation, { error: event.error });
       }
 
       if (event.done) {
-        if (currentBatch != null) {
-          const reducerResult = reducer(accumulation, { value: currentBatch });
+        if (batchedCount > 0) {
+          const reducerResult = reducer(accumulation, { value: batch.slice(0, batchedCount) });
           if (reducerResult instanceof Promise) {
             return reducerResult.then(({ accumulation }) => reducer(accumulation, { done: true }));
           }
@@ -23,12 +25,11 @@ function createBatchTransformer<T, AccumulationT>(count: number): Transformer<T,
         return reducer(accumulation, { done: true });
       }
 
-      currentBatch = currentBatch || [];
-      currentBatch.push(event.value);
-      if (currentBatch.length >= count) {
-        const sentBatch = currentBatch;
-        currentBatch = undefined;
-        return reducer(accumulation, { value: sentBatch });
+      batch[batchedCount] = event.value;
+      ++batchedCount;
+      if (batchedCount >= count) {
+        batchedCount = 0;
+        return reducer(accumulation, { value: batch.slice() });
       }
       return { accumulation, done: false };
     };
